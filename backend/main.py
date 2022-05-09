9211b7afda6d6e5dc1699e29977eac15560ecf82
John Park <mia@Johns-MacBook-Pro-3.local>
2022-05-08 23:12:29 -0700

    edit: initial read all users from db

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close
+
 @app.get('/')
-async def create_database():
-    return {'Soiree'}
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Users).all()
